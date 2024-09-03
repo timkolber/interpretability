69d5480cfcec20f1f6be1946a3940d12336bd7d7
kolber <kolber@cl.uni-heidelberg.de>
2024-09-03 18:44:02 +0200

    Update with GLM functionality

--- patchscopes/code/general_utils.py
+++ patchscopes/code/general_utils.py
@@ -23,114 +23,129 @@ import re
 
 import torch
 import transformers
+from transformers import PreTrainedTokenizer
+from typing import List, Tuple, Dict, Union, Optional
 
 
 class ModelAndTokenizer:
-  """An object to hold a GPT-style language model and tokenizer."""
+    """An object to hold a GPT-style language model and tokenizer."""
 
-  def __init__(
-      self,
-      model_name=None,
-      model=None,
-      tokenizer=None,
-      low_cpu_mem_usage=False,
-      torch_dtype=None,
-      use_fast=True,
-      device="cuda",
-      ):
-    if tokenizer is None:
-      assert model_name is not None
-      if "vicuna" in model_name or "llama" in model_name:
-        tokenizer = transformers.LlamaTokenizer.from_pretrained(model_name, use_fast=use_fast)
-      else:
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, use_fast=use_fast)
-    if model is None:
-      assert model_name is not None
-      model = transformers.AutoModelForCausalLM.from_pretrained(
-          model_name, low_cpu_mem_usage=low_cpu_mem_usage,
-          torch_dtype=torch_dtype
-          )
-      if device is not None:
-        model.to(device)
-      set_requires_grad(False, model)
-      model.eval()
-    self.tokenizer = tokenizer
-    self.model = model
-    self.device = device
-    self.layer_names = [
-        n
-        for n, _ in model.named_modules()
-        if (re.match(r"^(transformer|gpt_neox|model)\.(h|layers)\.\d+$", n))
-    ]
-    self.num_layers = len(self.layer_names)
+    def __init__(
+        self,
+        model_name=None,
+        model=None,
+        tokenizer=None,
+        low_cpu_mem_usage=False,
+        torch_dtype=None,
+        use_fast=True,
+        device="cuda",
+        cache_dir="/home/students/kolber/seminars/kolber/.cache/",
+    ):
+        if tokenizer is None:
+            assert model_name is not None
+            if "vicuna" in model_name or "llama" in model_name:
+                tokenizer = transformers.LlamaTokenizer.from_pretrained(
+                    model_name, use_fast=use_fast, cache_dir=cache_dir
+                )
+            else:
+                tokenizer = transformers.AutoTokenizer.from_pretrained(
+                    model_name, use_fast=use_fast, cache_dir=cache_dir
+                )
+        if model is None:
+            assert model_name is not None
+            model = transformers.AutoModelForCausalLM.from_pretrained(
+                model_name,
+                low_cpu_mem_usage=low_cpu_mem_usage,
+                torch_dtype=torch_dtype,
+                cache_dir=cache_dir,
+            )
+            if device is not None:
+                model.to(device)
+            set_requires_grad(False, model)
+            model.eval()
+        self.tokenizer = tokenizer
+        self.model = model
+        self.device = device
+        self.layer_names = [
+            n
+            for n, _ in model.named_modules()
+            if (re.match(r"^(transformer|gpt_neox|model)\.(h|layers)\.\d+$", n))
+        ]
+        self.num_layers = len(self.layer_names)
 
-  def __repr__(self):
-    """String representation of this class.
-    """
-    return (
-        f"ModelAndTokenizer(model: {type(self.model).__name__} "
-        f"[{self.num_layers} layers], "
-        f"tokenizer: {type(self.tokenizer).__name__})"
+    def __repr__(self):
+        """String representation of this class."""
+        return (
+            f"ModelAndTokenizer(model: {type(self.model).__name__} "
+            f"[{self.num_layers} layers], "
+            f"tokenizer: {type(self.tokenizer).__name__})"
         )
 
 
-def make_inputs(tokenizer, prompts, device="cuda"):
-  """Prepare inputs to the model."""
-  token_lists = [tokenizer.encode(p) for p in prompts]
-  maxlen = max(len(t) for t in token_lists)
-  if "[PAD]" in tokenizer.all_special_tokens:
-    pad_id = tokenizer.all_special_ids[
-        tokenizer.all_special_tokens.index("[PAD]")
-        ]
-  else:
-    pad_id = 0
-  input_ids = [
-      [pad_id] * (maxlen - len(t)) + t for t in token_lists]
-  attention_mask = [
-      [0] * (maxlen - len(t)) + [1] * len(t) for t in token_lists
-      ]
-  return dict(
-      input_ids=torch.tensor(input_ids).to(device),
-      attention_mask=torch.tensor(attention_mask).to(device),
-      )
+def make_inputs(
+    tokenizer: PreTrainedTokenizer, prompts: List[str], device: str = "cuda"
+) -> Dict[str, torch.Tensor]:
+    """This function tokenizes a list of text prompts, pads them to a uniform length,
+    creates attention masks, and converts the results into PyTorch tensors suitable for model input.
+
+    Args:
+        tokenizer (PreTrainedTokenizer): Tokenizer to use.
+        prompts (List[str]): List of prompts.
+        device (str, optional): Device to use. Defaults to "cuda".
+
+    Returns:
+        Dict[str, torch.Tensor]: Dictionary of input tensors.
+    """
+    token_lists = [tokenizer.encode(p) for p in prompts]
+    maxlen = max(len(t) for t in token_lists)
+    if "[PAD]" in tokenizer.all_special_tokens:
+        pad_id = tokenizer.all_special_ids[tokenizer.all_special_tokens.index("[PAD]")]
+    else:
+        pad_id = 0
+    input_ids = [[pad_id] * (maxlen - len(t)) + t for t in token_lists]
+    attention_mask = [[0] * (maxlen - len(t)) + [1] * len(t) for t in token_lists]
+    return dict(
+        input_ids=torch.tensor(input_ids).to(device),
+        attention_mask=torch.tensor(attention_mask).to(device),
+    )
 
 
 def decode_tokens(tokenizer, token_array):
-  if hasattr(token_array, "shape") and len(token_array.shape) > 1:
-    return [decode_tokens(tokenizer, row) for row in token_array]
-  return [tokenizer.decode([t]) for t in token_array]
+    if hasattr(token_array, "shape") and len(token_array.shape) > 1:
+        return [decode_tokens(tokenizer, row) for row in token_array]
+    return [tokenizer.decode([t]) for t in token_array]
 
 
 def find_token_range(tokenizer, token_array, substring):
-  """Find the tokens corresponding to the given substring in token_array."""
-  toks = decode_tokens(tokenizer, token_array)
-  whole_string = "".join(toks)
-  char_loc = whole_string.index(substring)
-  loc = 0
-  tok_start, tok_end = None, None
-  for i, t in enumerate(toks):
-    loc += len(t)
-    if tok_start is None and loc > char_loc:
-      tok_start = i
-    if tok_end is None and loc >= char_loc + len(substring):
-      tok_end = i + 1
-      break
-  return (tok_start, tok_end)
+    """Find the tokens corresponding to the given substring in token_array."""
+    tokens = decode_tokens(tokenizer, token_array)
+    whole_string = "".join(tokens)
+    char_loc = whole_string.index(substring)
+    loc = 0
+    tok_start, tok_end = None, None
+    for i, t in enumerate(tokens):
+        loc += len(t)
+        if tok_start is None and loc > char_loc:
+            tok_start = i
+        if tok_end is None and loc >= char_loc + len(substring):
+            tok_end = i + 1
+            break
+    return (tok_start, tok_end)
 
 
 def predict_from_input(model, inp):
-  out = model(**inp)["logits"]
-  probs = torch.softmax(out[:, -1], dim=1)
-  p, preds = torch.max(probs, dim=1)
-  return preds, p
+    out = model(**inp)["logits"]
+    probs = torch.softmax(out[:, -1], dim=1)
+    p, preds = torch.max(probs, dim=1)
+    return preds, p
 
 
 def set_requires_grad(requires_grad, *models):
-  for model in models:
-    if isinstance(model, torch.nn.Module):
-      for param in model.parameters():
-        param.requires_grad = requires_grad
-    elif isinstance(model, (torch.nn.Parameter, torch.Tensor)):
-      model.requires_grad = requires_grad
-    else:
-      assert False, "unknown type %r" % type(model)
+    for model in models:
+        if isinstance(model, torch.nn.Module):
+            for param in model.parameters():
+                param.requires_grad = requires_grad
+        elif isinstance(model, (torch.nn.Parameter, torch.Tensor)):
+            model.requires_grad = requires_grad
+        else:
+            assert False, "unknown type %r" % type(model)
